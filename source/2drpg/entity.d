16294017639de21dbba43d272f484e354c835d51
Vladde <v@vladde.net>
2016-01-17 20:53:42 +0100

    Start of nothing

---
+++ source/2drpg/entity.d
@@ -0,0 +1,24 @@
+enum EntityRace
+{
+  Human = 1;
+  Orc = 2;
+  Fishman = 4;
+  Elf = 8;
+  Dwarf = 16;
+}
+
+enum EntityType
+{
+}
+
+abstract class Entity
+{
+proteted:
+  int m_health;
+  int m_focus;
+
+  int m_x;
+  int m_y;
+
+  char m_skin = '?';
+}
---
+++ source/main.d
@@ -0,0 +1,32 @@
+import scone;
+
+void main()
+{
+
+}
+
+class Game
+{
+  void start()
+  {
+    running = true;
+
+    while(runing)
+    {
+      tick();
+      render();
+    }
+  }
+
+  void tick()
+  {
+
+  }
+
+  void render()
+  {
+
+  }
+
+  bool running;
+}
